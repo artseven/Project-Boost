7f12531e364fd14df650cd969621d34a4e4121de
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-10-29 19:39:02 -0400

    improvements for movement and collision detection

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -20,9 +23,21 @@ public class Rocket : MonoBehaviour {
         Rotate();
     }
 
+    private void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                //do nothing 
+                print("Ok");
+                break;
+            default: 
+                print("Dead");
+                break;
+        }
+    }
+
     private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying) {
                 audioSource.Play();
             }
@@ -33,10 +48,13 @@ public class Rocket : MonoBehaviour {
 
     private void Rotate() {
         rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         } else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
         rigidBody.freezeRotation = false; //resume physics control of rotation
     }
